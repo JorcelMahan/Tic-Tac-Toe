423ecf28d4b953a90c029d8b932904e0f33a6b85
Johan Marcel <johanmfc11@gmail.com>
2019-11-21 11:12:12 -0400

    game works, lack refactoring and no mark when everythig is full

--- game.js
+++ game.js
@@ -1,19 +1,75 @@
-const Player = (name) => {
+const Player = (name, marca, dom, turno, mark) => {
     const getName = () => name;
+    const getMarca = () => marca;
+    const getDom = () => dom;
+    const isTurn = () => turno;
+    const setTurno = () => turno = !turno;
+    const getMark = () => mark;
+    return {getName, getMarca, getDom, isTurn, setTurno, getMark};
 };
-const game = [
-    [0, 0, 0],
-    [0, 0, 0],
-    [0, 0, 0]
-];
+const Game = (player1, player2) => {
+    const grid = [
+        [0, 0, 0],
+        [0, 0, 0],
+        [0, 0, 0]
+    ];
+
+    const reset = () => {
+        for (let i = 0; i < grid.length; i++) {
+            for (let j = 0; j < grid.length; j++) {
+                grid[i][j] = 0;
+            }
+        }
+    };
+    const endGame = () => {
+        for (let i = 0; i < grid.length; i++) {
+            let sumRow = 0;
+            for (let j = 0; j < grid.length; j++) {
+                sumRow += grid[i][j];
+            }
+            if (sumRow === 3) return [true, player1];
+            if (sumRow === 6) return [true, player2];
+        }
+        return [false, null];
+    };
+    const draw = () => {
+        let results = [];
+        grid.forEach(e => {
+            let sum = e.reduce((a, b) => a + b);
+            results.push(sum !== 3 && sum !== 6 && !e.includes(0));
+        });
+        return !results.includes(false);
+    };
+    const whoesPlay = () => {
+        if (player1.isTurn()) {
+            player1.setTurno();
+            player2.setTurno();
+            return player1;
+        }
+
+        if (player2.isTurn()) {
+            player2.setTurno();
+            player1.setTurno();
+            return player2;
+        }
+    };
+    return {grid, whoesPlay, reset, draw, endGame};
+};
+let player1 = Player('player1', 'X', document.getElementById('player1'), true, 1);
+let player2 = Player('player2', 'O', document.getElementById('player2'), false, 2);
+let game = Game(player1, player2);
+
 const reset = document.querySelector('#reset');
 const start = document.querySelector('#start');
 let isStart = false;
+
 start.onclick = () => {
     isStart = true;
+    player1.getDom().style.border = `5px solid red`;
 };
 reset.onclick = () => {
     document.querySelectorAll('.board-box').forEach(el => el.textContent = '');
+    game.reset();
 };
 const render = () => {
     const board = document.querySelector('.board');
@@ -22,9 +78,19 @@ const render = () => {
 
 function touch(e) {
     if (isStart) {
-        e.target.textContent = 'X';
+        let player = game.whoesPlay();
+        e.target.textContent = player.getMarca();
         let [i, j] = e.target.dataset.box.split('');
-        game[i][j] = 1;
+        game.grid[i][j] = player.getMark();
+        if (player === player1) {
+            player1.getDom().style.border = `none`;
+            player2.getDom().style.border = `5px solid tomato`;
+        } else {
+            player2.getDom().style.border = `none`;
+            player1.getDom().style.border = `5px solid tomato`;
+        }
+        if (game.endGame()[0]) alert(`winner ${game.endGame()[1].getName()}`);
+        if (game.draw()) alert('draw');
     }
 }
 
